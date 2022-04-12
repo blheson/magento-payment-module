4f1f21b3c1bb8ffd16ce3f9f8ea20ad83ce0f0ed
unknown <blessingworl007@gmail.com>
2022-04-12 14:25:37 +0100

    Save merchant data

--- view/frontend/web/js/view/payment/method-renderer/rocketfuel.js
+++ view/frontend/web/js/view/payment/method-renderer/rocketfuel.js
@@ -29,12 +29,18 @@ define(
                 return this;
             },
             placingOrder: false,
+            placeOrderClicked: false,
             isActive: function () {
                 return true;
             },
             placeOrder: async function (data, event) {
+              
+                if (_this.placeOrderClicked === true) return;
                 // var self = this;
+                console.count("I am called", _this.placeOrderClicked);
                 var _this = this;
+                _this.placeOrderClicked = true;
+
                 if (event) {
                     event.preventDefault();
                 }
@@ -51,20 +57,37 @@ define(
                     paymentData.email = quote.guestEmail;
                 }
 
-                console.log(paymentData);
                 //     var quoteId = checkoutConfig.quoteItemData[0].quote_id;
 
-                _this.isPlaceOrderActionAllowed(false);
+                try {
+                    let results = await _this.init();
 
-                let results = await _this.init();
+                    if (results === false) {
 
-                console.log('init', results);
+                        console.log('engineplacingOrder trturen to false');
 
-                // return _this.customplaceOrder();
+                        _this.placingOrder = false;
 
-                return true;
+                        _this.placeOrderClicked = false;
+
+                    }
+
+                    console.log('Response from init', results);
+
+                    // return _this.customplaceOrder();
+
+                    return results;
+
+                } catch (error) {
+                    _this.placingOrder = false;
+
+                    _this.placeOrderClicked = false;
+                    console.error("There was an error with the place order flow,", error?.message);
+
+                }
+
             },
-      
+
             updateOrder: function (result) {
                 try {
 
@@ -96,13 +119,10 @@ define(
                         status = "wc-failed";
                     }
 
-                    // redirectOnSuccessAction.execute();
-                    // window.postMessage({ type: 'rocketfuel_data_update_custom_sdk', response: true });
+
                 } catch (error) {
 
                     console.error('Error from update order method', error);
-                    fullScreenLoader.stopLoader();
-                    engine.isPlaceOrderActionAllowed(true);
 
                 }
 
@@ -115,55 +135,14 @@ define(
                 window.addEventListener('message', (event) => {
 
                     switch (event.data.type) {
-                        case 'rocketfuel_data_update_custom_sdk':
-                            console.log('Event from rocketfuel_data_update_custom_sdk', event.data.response);
 
 
-                        case 'rocketfuel_iframe_close':
-                            console.log('closed');
-                            engine.isPlaceOrderActionAllowed(true);
-                            // engine.messageContainer.addErrorMessage({
-                            //     message: "iFrame closed"
-                            // });
-                            if (event.data.paymentCompleted === 1) {
-                                if (engine.placingOrder === true) return;
-                                console.log('This is closed and also validating');
-                                engine.placingOrder = true;
-                                if (additionalValidators.validate() &&
-                                    engine.isPlaceOrderActionAllowed() === true
-                                ) {
-                                    engine.isPlaceOrderActionAllowed(false);
-
-                                    engine.getPlaceOrderDeferredObject()
-                                        .done(
-                                            function () {
-                                                // self.afterPlaceOrder();
-
-                                                // if (self.redirectAfterPlaceOrder) {
-                                                redirectOnSuccessAction.execute();
-                                                // }
-                                            }
-                                        ).always(
-                                            function () {
-                                                engine.isPlaceOrderActionAllowed(true);
-                                            }
-                                        );
-
-                                    return true;
-                                }
-                            }
-                         
-                            fullScreenLoader.stopLoader();
-                            engine.placingOrder = false;
-
-                            break;
                         case 'rocketfuel_new_height':
 
                             engine.watchIframeShow = false;
 
                         case 'rocketfuel_result_ok':
-                            fullScreenLoader.stopLoader();
-                            console.log('Event from rocketfuel_result_ok', event.data.response);
+
 
                             if (event.data.response) {
                                 engine.updateOrder(event.data.response);
@@ -178,7 +157,7 @@ define(
             /**
              *  @override
              */
-             customOrders: async function () {
+            customOrders: async function () {
 
 
             },
@@ -203,14 +182,12 @@ define(
                         'id': item.item_id.toString()
                     }
                 });
-                console.log('shi card', checkoutConfig.selectedShippingMethod?.amount)
+
 
                 if (checkoutConfig.selectedShippingMethod?.amount || (checkoutConfig.totalsData.base_grand_total - productTotal > 0)) {
                     cart = [...cart, { name: 'Shipping', 'quantity': 1, price: checkoutConfig.selectedShippingMethod?.amount || productTotal, id: new Date().getTime().toString(), }];
                 }
 
-
-                console.log('Second card', cart);
 
                 let fd = new FormData();
 
@@ -220,7 +197,7 @@ define(
 
                 fd.append("cart", JSON.stringify(cart));
 
-                console.log("The cart is :", JSON.stringify(cart));
+
 
                 let response = await fetch(window.location.origin + '/rest/V1/rocketfuel-post-uuid', {
                     method: 'post',
@@ -231,7 +208,7 @@ define(
 
                 let parsedJson = JSON.parse(result);
                 // let parsedJson = result;
-                console.log('the response', parsedJson);
+
 
                 if (!parsedJson.uuid) {
                     return false;
@@ -248,37 +225,11 @@ define(
 
                 var paymentData = quote.billingAddress();
 
-                // var paystackConfiguration = checkoutConfig.payment.pstk_paystack;
 
-                // if (paystackConfiguration.integration_type == 'standard') {
-                //     this.redirectToCustomAction(paystackConfiguration.integration_type_standard_url);
-                // } else {
-
-                console.log('paymentData', paymentData);
-
-                let user_data;
-
-                if (checkoutConfig.isCustomerLoggedIn) {
-                    var customerData = checkoutConfig.customerData;
-
-                    paymentData.email = customerData.email;
-
-                    user_data = {
-                        first_name: customerData?.firstname || paymentData?.firstname,
-                        last_name: customerData?.lastname || paymentData?.lastname,
-                        email: customerData?.email || quote?.guestEmail,
-                    }
-
-                } else {
-
-                    var paymentData = quote.billingAddress();
-
-                    user_data = {
-                        first_name: paymentData?.firstname,
-                        last_name: paymentData?.lastname,
-                        email: paymentData?.email || quote?.guestEmail,
-                    }
-
+                let user_data = {
+                    first_name: paymentData?.firstname,
+                    last_name: paymentData?.lastname,
+                    email: paymentData?.email,
                 }
 
                 if (!user_data) return false;
@@ -297,7 +248,6 @@ define(
 
                     let userData = _this.getUserData();
 
-                    console.log('user data', userData);
 
                     let payload, response, rkflToken;
 
@@ -333,7 +283,7 @@ define(
                         }
 
                         try {
-                            console.log('details', userData.email, localStorage.getItem('rkfl_email'), payload);
+
 
                             if (userData.email !== localStorage.getItem('rkfl_email')) { //remove signon details when email is different
                                 localStorage.removeItem('rkfl_token');
@@ -373,7 +323,7 @@ define(
                     }
 
                     if (_this.rkflConfig) {
-                        console.log('data', _this.rkflConfig);
+
                         _this.rkfl = new RocketFuel(_this.rkflConfig); // init RKFL
 
                         resolve(true);
@@ -388,24 +338,77 @@ define(
             init: async function () {
 
                 let engine = this;
-                console.log('Start initiating RKFL');
 
-                try {
+                return new Promise(async (resolve, reject) => {
+                    try {
 
-                    let res = await engine.initRocketFuel();
-                    console.log(res);
+                        let res = await engine.initRocketFuel();
 
-                } catch (error) {
+                        console.log("Init rkfl", res);
 
-                    console.log('error from promise', error);
+                        window.addEventListener('message', (event) => {
+                            switch (event.data.type) {
+                                case 'rocketfuel_iframe_close':
+                                    console.log('Status of placing order', engine.placingOrder, event.data.paymentCompleted);
 
-                }
+                                    if (engine.placingOrder === true) return;
+                                    if (event.data.paymentCompleted === 1) {
 
-                console.log('Done initiating RKFL');
+                                        console.log('This is closed and also validating');
+                                        engine.placingOrder = true;
+                                        // engine.isPlaceOrderActionAllowed(false);
 
-                engine.windowListener();
+                                        if (additionalValidators.validate() &&
+                                            engine.isPlaceOrderActionAllowed() === true
+                                        ) {
+                                            engine.isPlaceOrderActionAllowed(false);
 
-                engine.startPayment();
+                                            engine.getPlaceOrderDeferredObject()
+                                                .done(
+                                                    function () {
+                                                        // self.afterPlaceOrder();
+
+                                                        // if (self.redirectAfterPlaceOrder) {
+                                                        redirectOnSuccessAction.execute();
+                                                        // }
+                                                    }
+                                                ).always(
+                                                    function () {
+                                                        console.log('always')
+                                                        engine.isPlaceOrderActionAllowed(true);
+
+                                                    }
+                                                );
+
+                                            resolve(true);
+                                            return;
+                                        }
+                                    } else {
+                                        fullScreenLoader.stopLoader();
+
+                                        resolve(false);
+                                    }
+
+
+
+                            }
+
+
+                        })
+                    } catch (error) {
+
+                        console.log('error from promise', error);
+                        resolve(false)
+
+                    }
+
+                    console.log('Done initiating RKFL');
+
+                    engine.windowListener();
+
+                    engine.startPayment();
+                })
+
 
             },
             startPayment: function () {
@@ -426,4 +429,4 @@ define(
     }
 );
 
-console.log('Shwoer 2');
+console.log('Shwoer 4');
